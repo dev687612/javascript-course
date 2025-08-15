8fc8bdf6ae212d0f36b39966f7ba22c4b97088c9
dev687612 <dingh6876@gmail.com>
2025-08-15 11:31:28 +0000

    done

---
+++ basics/dataType_02.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+/*
+DATA TYPES ->
+    1. number - 2^53
+    2. bigint - large number
+    3. string -" "
+    4. boolean - true/false
+    5. null
+    6. undefined
+    7. symbol - unique
+
+    object ->
+*/
+
+console.log(typeof("dev"));
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ basics/variables_01.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let accountEmail = "devsin345@gmail.com"
+var accountPassword = "1234"
+accountCity = "Chandigarh"
+let accountState
+
+// console.log(accountId);
+
+/*
+do not use var because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail , accountId ,accountPassword , accountCity , accountState])
+
